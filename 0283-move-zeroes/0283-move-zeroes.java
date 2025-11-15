f8ee46f6224d25717125f73f873c092de15c0610
Jayasurya Pazhani <110151956+Sparky1505@users.noreply.github.com>
2025-11-15 16:06:01 -0500

    Time: 2 ms (81.8%), Space: 47.6 MB (14.71%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,17 +1,61 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        
+    //   int first =0;
+    //   int second=1;
+    //   while(second<nums.length){
+    //     if(nums[first]==0){
+    //         int temp= nums[first];
+    //         nums[first]=nums[second];
+    //         nums[second]=temp;
+    //         first++;
+    //         second++;
 
-      int n = nums.length;
-      int[] num = new int[n];
-     int index=0;
-      for(int i =0;i<n;i++){
-        if(nums[i]!=0){
-            nums[index++]= nums[i];
+    //     }
+    //   }
+
+    // above -> tried with swap but won't when it has more than one 0
+
+    // int[] res = new int[nums.length];
+    // int index=0;
+    // for(int i =0;i<nums.length;i++){
+    
+    //     if(nums[i]>0){
+    //         res[index]=nums[i];
+    //         index++;
+    //     }
+    // }
+    // for(int i=0;i<nums.length;i++){
+    //     nums[i]=res[i];
+    // }
+
+    // above -> works with hint 1, tried with additional array then reassigning the value back
+
+
+    int i=0;
+    int index=0;
+    int count=0;
+    while(i<nums.length){
+        if(nums[i]==0){
+            count++;
         }
-      }
-      for(int i=index;i<n;i++){
-        nums[i]=0;
-      }
+        if(nums[i]!=0){
+            nums[index]= nums[i];
+            index++;
+        }
+        
+        i++;
+        
+    }
+    int zerolen= nums.length-count;
+    while(zerolen<nums.length){
+        nums[zerolen]=0;
+        zerolen++;
+    }
+   
+    
+
+
+
+
     }
 }
