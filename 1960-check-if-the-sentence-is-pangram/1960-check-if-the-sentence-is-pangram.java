21d9767cca5569f50f63e8f3b4d8e21767167f7d
Jayasurya Pazhani <110151956+Sparky1505@users.noreply.github.com>
2025-11-20 22:33:51 -0500

    Time: 4 ms (20.28%), Space: 43.2 MB (7.59%) - LeetHub

---
+++ 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+
+           Set set = new HashSet<>();
+           for(int i =0;i<sentence.length();i++){
+            set.add(sentence.charAt(i));
+           }
+           if(set.size()==26){
+            return true;
+           }
+           return false;
+
+        
+        
+    }
+}
