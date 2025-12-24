a1fe34805644055708a7b52d482e6d6f75a3e240
Jayasurya Pazhani <110151956+Sparky1505@users.noreply.github.com>
2025-12-23 22:48:00 -0500

    Time: 0 ms (100%), Space: 44 MB (17.4%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,6 +1,8 @@
 class Solution {
     public int search(int[] nums, int target) {
-
+            // this is not the optimal and in the required constraint
+            // it has to be in log n but in O(n)
+            // to learn the optimize approch and implement it
         int result =-1;
         for(int i=0;i<nums.length;i++ ){
             if(nums[i]==target){
