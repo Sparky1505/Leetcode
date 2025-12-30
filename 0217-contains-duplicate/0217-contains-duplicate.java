ac6516bdd24f02f6eac655e115c8e1aa93eaf771
Jayasurya Pazhani <110151956+Sparky1505@users.noreply.github.com>
2025-12-29 22:06:30 -0500

    Time: 34 ms (5.08%), Space: 96.2 MB (22.01%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -3,19 +3,25 @@ class Solution {
 
         int n = nums.length;
 
-        HashMap<Integer,Integer> map1 = new HashMap<>();
-        int count=0;
-        for(int i =0;i<n;i++){
+        HashMap<Integer,Integer> map= new HashMap<>();
+
+        for(int i=0;i<n;i++){
             int num = nums[i];
-            map1.put(num,map1.getOrDefault(num,0)+1);
+
+          
+                map.put(num,map.getOrDefault(num,0)+1);
+            
+
         }
 
-        for(int num: map1.values()){
+        for(int num: map.values()){
             if(num>1){
                 return true;
             }
         }
         return false;
+
+
         
     }
 }
