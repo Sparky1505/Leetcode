98da1e2456933525f128a91e16e3e7bc402504e5
Jayasurya Pazhani <jsurya733@gmail.com>
2026-01-04 16:59:00 -0500

    Time: 10 ms (79.44%), Space: 46.3 MB (68.78%) - LeetHub

--- 1390-four-divisors/1390-four-divisors.java
+++ 1390-four-divisors/1390-four-divisors.java
@@ -1,11 +1,11 @@
 class Solution {
     public int sumFourDivisors(int[] nums) {
 
-
         int n = nums.length;
-        int result=0;
 
-        for(int i =0;i<n;i++){
+     
+       int result=0;
+         for(int i =0;i<n;i++){
             int sum =0;
             int count=0;
             for(int j =1;j*j<=nums[i];j++){
@@ -29,6 +29,5 @@ class Solution {
                 }
         }
         return result;
-        
     }
 }
