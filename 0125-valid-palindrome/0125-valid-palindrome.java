aa82b8df7ccbd57dabffee40adeae259cee61f35
Jayasurya Pazhani <jsurya733@gmail.com>
2026-01-08 19:30:35 -0500

    Time: 14 ms (32.2%), Space: 46.3 MB (33.2%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -3,15 +3,17 @@ class Solution {
 
         
         s=s.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
-        int j=s.length()-1;
-        for(int i =0;i<s.length();i++){
-            if(s.charAt(i)!=s.charAt(j)){
+        int left =0;
+        int right = s.length()-1;
+
+        while(left<right){
+            if(s.charAt(left)!=s.charAt(right)){
                 return false;
             }
-            j--;
+            left++;
+            right--;
         }
         return true;
-      
 
     }
 }
