54482303e7cfe1877612bda932d669a8eebe59f7
Jayasurya Pazhani <jsurya733@gmail.com>
2026-01-14 18:49:26 -0500

    Time: 22 ms (31.26%), Space: 81 MB (74.21%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,17 +1,19 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
 
-        int n = nums.length;
-        HashSet<Integer> numbers = new HashSet<>();
-        for(int i =0;i<n;i++){
-            if(numbers.contains(nums[i])){
-                return true;
-            }
-            numbers.add(nums[i]);
-        }
-     
-        return false;
+       	int n = nums.length;
+		
+		
+		HashSet<Integer> set1= new HashSet<>();
+		for(int i =0;i<n;i++) {
+			int num = nums[i];
+			set1.add(num);
+		}
+		if(n!=set1.size()) {
+			return true;
+		}
 
+		return false;
 
         
     }
